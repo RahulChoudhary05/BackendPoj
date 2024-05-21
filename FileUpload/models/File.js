499061b9e700dc456712d9b7dc61cff4f29c786a
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2024-05-21 15:44:12 +0530

    mail send by postman and fetch the data from mongodb

--- FileUpload/models/File.js
+++ FileUpload/models/File.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 const fileSchema = new mongoose.Schema({
   name: {
@@ -14,6 +16,33 @@ const fileSchema = new mongoose.Schema({
   email: {
     type: String,
   },
+});
+
+//post middleware
+fileSchema.post("save", async function (doc) {
+  try {
+    console.log("DOC => ", doc);
+
+    //transporter
+    let transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
+
+    //sent mail
+    let info = await transporter.sendMail({
+      from: "Rahul Choudhary | Developer",
+      to: doc.email,
+      subject: "New file uploaded on Cloudinary 😎",
+      html: `<h2> Hello ${doc.name}</h2> <p> New File Uploaded <p> File URL : ${doc.imageUrl} ${doc.videoUrl}`,
+    })
+    console.log("INFO => ", info);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 const File = mongoose.model("File", fileSchema);
