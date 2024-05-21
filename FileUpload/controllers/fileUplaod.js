8d682c6ff790d56008950036a5d52459468f2acd
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2024-05-21 11:16:54 +0530

    complete videoUpload using postman and all data store in mongodb and video store in cloudinary

--- FileUpload/controllers/fileUplaod.js
+++ FileUpload/controllers/fileUplaod.js
@@ -34,6 +34,8 @@ function isFileTypeSupported(type, supportedTypes) {
 
 async function uploadFileTocloudinary(file, folder) {
   const options = { folder };
+  console.log("Temp file path => ", file.tempFilePath);
+  options.resource_type = "auto";
   return await cloudinary.uploader.upload(file.tempFilePath, options);
 }
 
@@ -51,7 +53,7 @@ exports.imageUpload = async (req, res) => {
     const supportedTypes = ["jpg", "png", "jpeg "];
     const fileType = file.name.split(".")[1].toLowerCase();
     console.log("File type => ", fileType);
-    
+
     if (!isFileTypeSupported(fileType, supportedTypes)) {
       return res.status(400).json({
         successs: false,
@@ -65,11 +67,11 @@ exports.imageUpload = async (req, res) => {
 
     //entery save in db
     const fileData = await File.create({
-        name,
-        tags,
-        email,
-        imageUrl: response.secure_url,
-    })
+      name,
+      tags,
+      email,
+      imageUrl: response.secure_url,
+    });
 
     res.json({
       successs: true,
@@ -79,8 +81,57 @@ exports.imageUpload = async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(400).json({
-        successs:false,
-        message:"Something went wrong",
-    })
+      successs: false,
+      message: "Something went wrong",
+    });
+  }
+};
+
+//video upload
+exports.videoUpload = async (req, res) => {
+  try {
+    //data fetch
+    const { name, tags, email } = req.body;
+    console.log(name, tags, email);
+
+    const file = req.files.videoFile;
+    console.log("VideoFile => ", file);
+
+    //validation
+    const supportedTypes = ["mp4", "mov"];
+    const fileType = file.name.split(".")[1].toLowerCase();
+    console.log("File type => ", fileType);
+
+    //add upper limit of 5 mb for video
+    if (!isFileTypeSupported(fileType, supportedTypes)) {
+      return res.status(400).json({
+        successs: false,
+        message: "File format not supported",
+      });
+    }
+
+    //file format supported
+    const response = await uploadFileTocloudinary(file, "FileUpload");
+    console.log(response);
+
+    //entery save in db
+    const fileData = await File.create({
+      name,
+      tags,
+      email,
+      imageUrl: response.secure_url,
+    });
+
+    res.json({
+      successs: true,
+      imageUrl: response.secure_url,
+      message: "Video successfully uploaderd",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      successs: false,
+      message: "Something went wrong",
+    });
   }
 };
--- FileUpload/routes/FileUpload.js
+++ FileUpload/routes/FileUpload.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const routes = express.Router();
-const { localFileUpload, imageUpload } = require("../controllers/fileUplaod");
+const { localFileUpload, imageUpload, videoUpload } = require("../controllers/fileUplaod");
 
 //routers
-// routes.post("/videoUpload", videoUpload);
-// routes.post("/imageReducerUpload", imageReducerUpload);
 routes.post("/localFileUpload", localFileUpload);
 routes.post("/imageUpload", imageUpload);
+routes.post("/videoUpload", videoUpload);
 
 module.exports = routes;
